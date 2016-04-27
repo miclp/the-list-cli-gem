fae1f159b7239c7195834a19a259716ef640b364
miclp <michaelpeimani@gmail.com>
2016-04-26 22:32:10 -0700

    Comment out venue class

--- lib/venue.rb
+++ lib/venue.rb
@@ -1,12 +1,9 @@
-# Venue class
-
-class Venue
-
-  # attributes - :name, :address, :age_restrictions, :alcohol_license
-  attr_accessor :name, :address
-
-  # shows []
-
-
-
-end
+# # Venue class
+#
+# class Venue
+#
+#   # attributes - :name, :address, :age_restrictions, :alcohol_license
+#   attr_accessor :name, :address
+#
+#   # shows []
+# end
